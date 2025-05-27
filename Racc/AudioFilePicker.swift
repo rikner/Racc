a09a9dececd1fd9e309a2d4b94972cdd7ed732b6
Erik Werner <rikner@web.de>
2025-05-27 17:13:22 +0200

    AudioFilePicker: update button text and streamline button implementation

--- Racc/AudioFilePicker.swift
+++ Racc/AudioFilePicker.swift
@@ -3,24 +3,24 @@ import FilePicker
 
 struct AudioFilePicker: View {
     @Binding var selectedFileUrl: URL? // Changed to bind a single URL
-    var buttonText: String = "Select File"
+    var buttonText: String = "Open"
 
     @State private var filePickerOpen = false
     @State private var filePickerFiles: [URL] = [] // Still used by the .filePicker modifier
 
     var body: some View {
-        VStack {
-            Button(buttonText, systemImage: "square.and.arrow.down") {
-                filePickerOpen.toggle()
-            }
+        Button {
+            filePickerOpen.toggle()
+        } label: {
+            Image(systemName: "square.and.arrow.down")
+                .frame(width: 64, height: 24)
         }
-        .padding()
         .filePicker(
             isPresented: $filePickerOpen,
             files: $filePickerFiles, // The modifier expects a binding to an array
             types: [.wav, .mp3, .aiff] // Added more common audio types
         )
-        .onChange(of: filePickerFiles) { newFiles in
+        .onChange(of: filePickerFiles) { _, newFiles in
             if let firstFile = newFiles.first {
                 selectedFileUrl = firstFile
                 // If you only ever want one file, clear the array after selection
